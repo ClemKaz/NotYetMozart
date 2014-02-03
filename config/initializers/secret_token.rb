cadcd88f41e5a6ccfa43691a0b212a80161cc7ee
Clemens Kazda <clemkaz@Clemenss-MacBook-Pro.local>
2014-02-03 10:03:03 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotYetMozart::Application.config.secret_token = 'e7c69eb1bab9fc84d71bb451836fa395ffb087aa906cada26ce57299be7d1c0fa127d7c50b13fc7a5cab3563cfeed85d943a578d50ebb7f43967e39064e04b3f'
